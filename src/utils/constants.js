68219a074c9dd8e4fceb18a0f3115bc8d51e638c
Vicky <vicky9060993097@gmail.com>
2023-04-08 13:40:59 +0530

    Added firebase config

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
-export const API_KEY = process.env.APIKEY;
+export const API_KEY = process.env.REACT_APP_APIKEY;
 export const TMBD_BASE_URL = "https://api.themoviedb.org/3";
--- src/utils/firebase-config.js
+++ src/utils/firebase-config.js
@@ -1,6 +1,14 @@
 import { initializeApp } from "firebase/app";
 import {getAuth} from "firebase/auth";
-const firebaseConfig =process.env.FIREBASE;
+const firebaseConfig = {
+  apiKey: "AIzaSyBxgIet_96WGyn8DGfufNzWx9-ropj5hfE",
+  authDomain: "netflix-1001.firebaseapp.com",
+  projectId: "netflix-1001",
+  storageBucket: "netflix-1001.appspot.com",
+  messagingSenderId: "216395398612",
+  appId: "1:216395398612:web:eba0e1c17992654c45aa41",
+  measurementId: "G-YXBB18WN50"
+  };
 
 const app = initializeApp(firebaseConfig);
 export const firebaseAuth = getAuth(app);
